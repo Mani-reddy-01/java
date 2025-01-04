f0d7261611ae34e59bbd0a390367b90354a0cb60
Mani <manireddymvr03@gmail.com>
2025-01-04 21:23:14 +0530

    first commit

---
+++ src/Session_4.java
@@ -0,0 +1,47 @@
+public class Session_4 {
+    public static void main(String[] args) {
+        int a = (int)20.9;
+        System.out.println(a);
+        //double
+        double d1=90;
+        System.err.println(d1);
+        //float
+        float f1=2.9F;
+        System.out.println(f1);
+        //double 
+        double d2=10/3;
+        System.out.println(d2);
+        //float
+        float f2=10.0f/3;
+        System.out.println(f2);
+        //arthematic expressions + - * /
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        //comparision operations 
+        //<,>,<=,>=,!=,==
+        System.out.println(7==8);
+        System.out.println(7!=8);
+        System.out.println(2<2);
+        if(5>8) {
+            System.out.print("5 is greater than 1");
+        } else{
+            System.out.println("not greater");
+        }
+        // create two variables
+        //x and y
+        //to check if x is greater than y 
+        //if it is, print x greater 
+        //else print y is greater
+
+        int x=7;
+        int y=8;
+        if(x>y){
+            System.out.println("X IS GREATER");
+        }else {
+            System.out.println("Y IS GREATER");
+        }
+
+    }
+}
