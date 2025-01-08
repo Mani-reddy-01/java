7efd7b871f1c226d6adf80cbd1c5ddd602e9adbd
Mani-reddy-01 <manireddymvr03@gmail.com>
2025-01-08 18:50:32 +0530

    Add files via upload

---
+++ Day_6_increase.java
@@ -0,0 +1,11 @@
+    // increasing order
+public class Day_6_increase {
+    public static void main(String[] args) {
+for(int i=1;i<=4;i++){
+    for(int j=1;j<=i;j++){
+        System.out.print(" * ");
+    }
+    System.out.println("");
+}
+    }
+}
